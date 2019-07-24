9d82714560c987b4bacc0dfb7d144d2dbd0fe02f
razi21-meet <razi21@meet.mit.edu>
2019-07-24 03:30:05 -0400

    Stopping here

---
+++ Unit 4: Turtle Lab.py
@@ -0,0 +1,17 @@
+import turtle
+#the square + triangle
+turtle.shape('turtle')
+turtle.goto(200,0)
+turtle.goto(100,200)
+turtle.goto(0,0)
+finn = turtle.clone()
+finn.shape('square')
+finn.goto(200,0)
+finn.goto(200,200)
+finn.goto(0,200)
+finn.goto(0,0)
+finn.goto(200,0)
+turtle.mainloop()
+
+
+ 
